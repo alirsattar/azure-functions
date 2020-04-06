7677d4eed5d75e7544fbd83d0a0785e772a80128
Ali Sattar <alirsattar@gmail.com>
2020-04-06 10:55:46 -0400

    Testing package deploy

--- TestFunction1/index.js
+++ TestFunction1/index.js
@@ -1,16 +1,25 @@
 module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+	context.log('JavaScript HTTP trigger function processed a request.');
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
-    }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
-    }
+	// const body = JSON.parse( JSON.stringify( req.body ) );
+
+	// console.log('************************************');
+	// console.log( req.body.name );
+	// console.log('************************************');
+
+	console.log('*** Testing updating Azure Function from package');
+
+	if (req.query.name || (req.body && req.body.name)) {
+		context.res = {
+			// status: 200, /* Defaults to 200 */
+			body: "Hello " + (req.query.name || req.body.name)
+		};
+	} else {
+		context.res = {
+			status: 400,
+			body: `Please pass a name on the query string or in the request body`
+		};
+	};
+
+	context.done();
 };
